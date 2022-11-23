da860e9d2459a77eaa98e9f2ccee1297b367e11b
prakhar12330 <prakhar12330@gmail.com>
2022-11-23 17:54:11 +0530

    MyFirstCommit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>Amma's Pickle</h2>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <p>
+      awesome place to know about diffrent types of pickles.
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,123 @@
+export const blogList = [
+  {
+    id: 1,
+    title: 'Dill Pickles',
+    category:"Cucumber",
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Dill is a popular variety of cucumber pickles that comes in two main types: Genuine and Kosher.When you think or talk about pickles, the first thing that probably pops up into your mind is the dill variety, the most common one being genuine dill pickles. The pickles are whole cucumbers packed with dill seeds.They are prepared from a traditional pickling method which is also the simplest one — pickles are covered in flavored vinegar and then stored on a shelf at room temperature. Most people prefer to snack on genuine dill pickles directly from the jar, but they can also be added to a juicy burger, or hot dog, or savored simply as a tasty dip.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover:'/assets/images/Dill-Pickles-in-an-Open-Jar-2-20-6-870x582.jpg.webp'
+  },
+  {
+    id: 2,
+    title: 'Sweet Pickles',
+    category: 'Onion',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover: '/assets/images/Sweet-Pickles-May102020-min.jpg.webp',
+  },
+  {
+    id: 3,
+    title: 'Gherkin Pickles',
+    category: 'Gourd',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover: '/assets/images/3.jpg',
+  },
+  {
+    id: 4,
+    title: 'Cinnamon Pickles',
+    category: 'cinnamon',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover: '/assets/images/4.jpg',
+  },
+  {
+    id: 5,
+    title: 'Lime Pickle',
+    category: 'Lime',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover: '/assets/images/5.jpg',
+  },
+  {
+    id: 6,
+    title: 'Hungarian Pickles',
+    category: 'Cucumber',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover:
+      '/assets/images/6.jpg',
+  },
+  {
+    id: 7,
+    title: 'Keto Pickle: Kosher Baby Dill Pickles',
+    category: 'Kosher',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover: '/assets/images/7.jpg',
+  },
+  {
+    id: 8,
+    title: 'Shawarma Pickles: Pickled or Fermented Turnips & Beets',
+    category: 'Shawarma',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover: '/assets/images/8.jpg',
+  },
+  {
+    id: 9,
+    title: 'Deep-Fried Pickles',
+    category: 'Lemon',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover: '/assets/images/9.jpg',
+  },
+  {
+    id: 10, 
+    title: 'Pickles in Egg Salad: Sweet Pickle Relish',
+    category: 'Egg',
+    subCategory: ['Sour', 'Sweet', 'Salty'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Amma',
+    authorAvatar: '/assets/images/author.webp',
+    createdAt: 'Dec 11, 2022',
+    cover: '/assets/images/10.jpg',
+  },
+];
